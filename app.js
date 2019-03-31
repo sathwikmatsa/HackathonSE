f859d58ce89563738d47ca16f902858954af5db6
Sathwik Matsa <sathwikmatsa@gmail.com>
2019-04-01 00:17:49 +0530

    setup authentication and session management

--- app.js
+++ app.js
@@ -1,15 +1,66 @@
+const config = require('./config')
+const db = require('./database/queries');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
+const uuid = require('uuid/v4');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const app = express();
 const port = 5000;
+
+passport.use(new LocalStrategy(
+    {usernameField: 'email'},
+    (email, password, done) => {
+        db.getUserByEmail(email, (user) => {
+            if (user){
+                bcrypt.compare(password, user.password, function(err, res) {
+                    if(res) {
+                        return done(null, user);
+                    } else {
+                        return done(null, false, { message: 'Invalid credentials.' });
+                    }
+                });
+            } else {
+                return done(null, false, { message: 'Invalid credentials.' });
+            }
+        });
+    }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user.user_id);
+});
+
+passport.deserializeUser((id, done) => {
+    db.getUserByEmail(id, (user) => {
+        done(null, user);
+    });
+});
 
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
-    extended: true,
+      extended: true,
   })
 );
+
+app.use(session({
+    genid: (req) => {
+        return uuid();
+    },
+    store: new FileStore(),
+    secret: config.secret,
+    resave: false,
+    saveUninitialized: true,
+    name: 'hhcookie'
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
@@ -17,28 +68,70 @@ app.set('views', './views');
 
 app.use(express.static('public'));
 
-app.get('/', (request, response) => {
-  response.render("index");
+app.get('/', (req, res) => {
+    if( !req.isAuthenticated() )
+        res.render("index");
+    else
+        res.redirect('/home');
 });
 
-app.get('/login', (request, response) => {
-  response.render("login");
+app.get('/login', (req, res) => {
+    if( !req.isAuthenticated() )
+        res.render("login");
+    else
+        res.redirect('/home');
 });
 
-app.post('/login', (request, response) => {
-  console.log(request.body.email, request.body.password);
-  response.send("received login request! email:"+request.body.email+" password:"+request.body.password);
+app.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (err) { return next(err); }
+        if (!user) {
+            return res.render('login', {
+            'message': '*Invalid credentials'
+            });
+        }
+        req.logIn(user, function(err) {
+            if (err) { return next(err); }
+            return res.redirect('/home');
+        });
+    })(req, res, next);
 });
 
-app.get('/signup', (request, response) => {
-  response.render("signup");
+app.get('/signup', (req, res) => {
+    if( !req.isAuthenticated() )
+        res.render("signup");
+    else
+        res.redirect('/home');
 });
 
-app.post('/signup', (request, response) => {
-  console.log(request.body.email, request.body.password);
-  response.send("received signup request! email:"+request.body.email+" password:"+request.body.password);
+app.post('/signup', db.createUser);
+
+app.get('/home', (req, res) => {
+    if(req.isAuthenticated()) {
+        db.getUserByEmail(req.session.passport.user, (user) => {
+            if (user){
+                res.render("home", {
+                    "fname": user.first_name,
+                    "lname": user.last_name
+                });
+            }
+        });
+    } else {
+        res.redirect('/')
+    }
+})
+
+app.get('/logout', function (req, res) {
+    req.session.destroy(function(err) {
+        if (err) {
+            console.error(err);
+        } else {
+            res.clearCookie('hhcookie');
+            res.redirect('/');
+        }
+    });
 });
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
+    console.log(`App running on port ${port}.`);
 });
---
+++ database/queries.js
@@ -0,0 +1,48 @@
+const config = require("./../config");
+const bcrypt = require('bcrypt');
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: config.db.user,
+    host: config.db.host,
+    database: config.db.database,
+    password: config.db.password,
+    port: config.db.port
+});
+
+const getUserByEmail = (email, callback) => {
+    pool.query('SELECT * FROM person WHERE user_id = $1', [email], (error, results) => {
+        if (error){
+            throw error;
+        }
+        callback(results.rows[0]);
+    })
+}
+
+const createUser = (request, response) => {
+    const {email, password, fname, lname, phone, gender} = request.body;
+    const age = parseInt(request.body.age);
+    //TODO: refactor this piece
+    getUserByEmail(email, (data) => {
+        if (data){
+            response.render('signup', {
+                "error": "A user with that email already exists. Please try a different email, or <a href='/login'>log in</a> to your existing account."
+            });
+            return;
+        }
+
+        bcrypt.hash(password, 10, (err, hash) => {
+
+            pool.query('INSERT INTO person (user_id, password, first_name, last_name, ph_no, age, gender, karma) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [email, hash, fname, lname, phone, age, gender, 100], (error, results) => {
+                if (error) {
+                    throw error;
+                }
+                response.redirect("/home");
+            })
+        });
+    });
+}
+
+module.exports = {
+  getUserByEmail,
+  createUser
+}
